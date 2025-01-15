8f55851ffecefe57ec86aca4abbf48a544af12c4
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2025-01-14 19:09:35 -0600

    More beta testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image src={logoWhite} alt="logo" className="logo-white" />
         <Image src={logoBlack} alt="logo" className="logo-black" />
-        Lo sentimos, este sitio web se encuentra bajo<br/> 
-        construcción. Comunícate con nosotros en<br/>
-        los siguientes enlaces:
+        Beta testing...
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
