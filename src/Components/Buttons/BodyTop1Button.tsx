6586aa7a534a4cc5ef7280902a853619057e867b
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2025-01-19 19:47:19 -0600

    Fix marging top

--- src/Components/Buttons/BodyTop1Button.tsx
+++ src/Components/Buttons/BodyTop1Button.tsx
@@ -12,11 +12,20 @@ interface BodyTop1Buttonprops {
 
 const BodyTop1Button: React.FC<BodyTop1Buttonprops> = ({ label, ContactLink, ButtonColor, TextColor }) => {
     return (
-        <Button href={ContactLink}
-                target="_blank"
-                rel="noreferrer"
-                style={{ textDecoration:'none', backgroundColor: ButtonColor, color: TextColor, maxWidth: '100%', whiteSpace: 'normal', wordWrap: 'break-word', fontSize: 'calc(0.75rem + 0.5vw)' }}
-                className="BodyB  text-center">
+        <Button
+            href={ContactLink}
+            target="_blank"
+            rel="noreferrer"
+            style={{ 
+                textDecoration:'none',
+                backgroundColor: ButtonColor,
+                color: TextColor,
+                maxWidth: '100%',
+                whiteSpace: 'normal',
+                wordWrap: 'break-word',
+                fontSize: 'calc(0.75rem + 0.5vw)'
+            }}
+            className="BodyB text-center">
             {label}
         </Button>
     );
